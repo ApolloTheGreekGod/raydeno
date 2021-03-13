c5bbdaeaeb6f5dbb7e60dacc6a9daa482f7d8d65
ApolloTheGreekGod <63588755+ApolloTheGreekGod@users.noreply.github.com>
2021-03-13 22:57:57 +0800

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $("navbutton1").click(function(){
+    $(".navbutton1").click(function(){
 
-        if($("button").toggleClass("navbutton1").text() == "☰"){
-        $("button").toggleClass("navbutton1").text("🞬");
+        if($(".navbutton1").text() == "☰"){
+        $(".navbutton1").text("🞬");
     }
     else{
-        $("button").toggleClass("navbutton1").text("☰");
+        $(".navbutton1").text("☰");
     }
 
     $("li").toggle("slow");
