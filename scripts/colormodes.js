8d259b72f2cef9d686a39a95fe39c0cd28e2e869
ApolloTheGreekGod <63588755+ApolloTheGreekGod@users.noreply.github.com>
2021-10-26 18:23:13 +0800

    Updated Navbar

--- scripts/colormodes.js
+++ scripts/colormodes.js
@@ -1,4 +1,10 @@
 function colormode() {
     var element = document.body;
     element.classList.toggle("lightmode");
+    var colory = document.getElementById("ColorModes");
+    if (colory.innerHTML === "Click for Light Mode" ) {
+        colory.innerHTML = "Click for Dark Mode";
+    } else {
+        colory.innerHTML = "Click for Light Mode";
+    }
     }
